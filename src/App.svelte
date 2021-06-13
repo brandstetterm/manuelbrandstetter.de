7bd759d415cba442b2fe5ba9633b7c74cab1d119
Manuel Brandstetter <accounts@manuelbrandstetter.de>
2021-06-13 11:27:04 +0200

    Modify default svelte template

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,30 @@
-<script>
-	export let name;
-</script>
-
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Hello,</p>
+	<h1>I'm Manuel</h1>
+	<p>Computer Science Student</p>
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
+		width: max-content;
+	}
+
+	p {
+		width: max-content;
+		color: #adadad;
+		margin: 0;
+		letter-spacing: 0.02em;
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #0065bd;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		width: max-content;
+		margin: 5px 0;
 	}
 </style>
